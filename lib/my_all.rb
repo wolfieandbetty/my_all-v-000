13d92cccd80eba0edbcee1a05dd230ac4a350b45
Cat Norris <cannenorris@yahoo.com>
2017-12-01 03:51:23 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+  i=0
+  while i < collection.length
+    yield(collection[i])
+    binding.pry
+    i+=1
+  end
+
+  if block_return_values.include? (false)
+    false
+  else
+    true
+  binding.pry
+  end
 
 end
+
+my_all?([1,2,3]) {|i| i < 2}
